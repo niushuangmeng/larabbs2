127598f687e1da430446b8dd41398593f041ca0b
niushuangmeng <niushuangmeng@gmail.com>
2019-09-06 06:49:46 +0000

    邮箱认证

--- config/services.php
+++ config/services.php
@@ -35,7 +35,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => ::class,
+        'model' => App\Models\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
         'webhook' => [
