58193ca945801bc5eb50c200ad6aed76900064b4
niushuangmeng <niushuangmeng@gmail.com>
2019-09-16 05:43:30 +0000

    修复N+1问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,7 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		$topics = Topic::with('user','category')->paginate(30);
 //		dd($topics);
 		return view('topics.index', compact('topics'));
 	}
