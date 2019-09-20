2e7026dc7ef78735c746ebfc0af36537964a8c11
niushuangmeng <niushuangmeng@gmail.com>
2019-09-20 03:29:39 +0000

    用户可以发表评论

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,14 +21,13 @@ class TopicObserver
         //
     }
 
-    public function saving(Topic $topic){
+    public function saving(Topic $topic)
+    {
+        // XSS 过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
 
-        $topic->body = clean($topic->body,'user_topic_body');
+        // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
-//        if(!$topic->slug){
-////            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
-//            dispatch(new TranslateSlug($topic));
-//        }
     }
 
     public function saved(Topic $topic)
